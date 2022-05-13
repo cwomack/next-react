c3b62de2ce496433cc5336501c8e4eac6e3af0b6
WomackCodes <chris@womack.codes>
2022-05-12 23:55:43 -0700

    Test Component

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -6,3 +6,9 @@ export default function Home() {
   const router = useRouter();
   return <h1> This page is for product {JSON.stringify(router.query)}</h1>;
 }
+
+class Square extends React.Component {
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
+}
